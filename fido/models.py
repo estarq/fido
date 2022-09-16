9b111b32f2a74385e206a7f3417f0dde17d8ae30
estarq <61628924+estarq@users.noreply.github.com>
2022-09-16 23:07:43 +0200

    Create and register a Dog model

--- fido/models.py
+++ fido/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Dog(models.Model):
+    SEX = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    name = models.CharField(max_length=15)
+    sex = models.CharField(max_length=1, choices=SEX)
+    short_desc = models.CharField(max_length=100)
+    desc = models.TextField()
+    img = models.ImageField()
