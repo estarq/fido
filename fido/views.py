d2a80b4edda2f8129b6eca421c4a592794d8531b
estarq <61628924+estarq@users.noreply.github.com>
2022-09-17 17:11:21 +0200

    Display dogs

--- fido/views.py
+++ fido/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from fido.models import Dog
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'fido/index.html')
+    dogs = Dog.objects.all()
+    return render(request, 'fido/index.html', {'dogs': dogs})
